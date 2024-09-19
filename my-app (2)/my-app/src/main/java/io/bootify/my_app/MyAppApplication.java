e449bf8090722ff940a8dab5fbc787b1fa02be67
jmartter <150058251+jmartter@users.noreply.github.com>
2024-09-19 18:44:31 +0200

    Comienzo docker

--- my-app (2)/my-app/src/main/java/io/bootify/my_app/MyAppApplication.java
+++ my-app (2)/my-app/src/main/java/io/bootify/my_app/MyAppApplication.java
@@ -1,8 +1,9 @@
 package io.bootify.my_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyAppApplication {
@@ -11,4 +12,10 @@ public class MyAppApplication {
         SpringApplication.run(MyAppApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Aplicación iniciada correctamente Fernando!");
+        };
+    }
 }
